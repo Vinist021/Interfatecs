c19ee786b45665081f391fff122edbd0bef41d30
Vinicius Teixeira <vinist.002@gmail.com>
2025-05-20 23:03:26 -0300

    Problema A - 2025

---
+++ Interfatecs 2025/Resoluções/AAA.py
@@ -0,0 +1,29 @@
+while True:
+    entrada = input()
+    nums = []
+
+    if entrada == '*':
+        break
+
+    for i in entrada:
+        if i == '*':
+            nums.append(0)
+        elif i == '.':
+            nums.append(1)
+        elif i == '-':
+            nums.append(5)
+        elif i == ' ':
+            nums.append(' ')
+
+    base = 0
+    numTotal = 0
+    nums = nums[::-1]
+
+    for i in nums:
+        if i == ' ':
+            base += 1
+            continue
+
+        numTotal += i * (20 ** base)
+
+    print(numTotal)
